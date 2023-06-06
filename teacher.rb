3e8617ae0f69de8ca8f2d38b567caccf0464a58e
FrankMugagga <frank.mugagga@gmail.com>
2023-06-06 17:10:30 +0300

    Create teacher file which inherits from person class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Teacher < Person
+    attr_reader :specialization
+    def initialize(age, name: "unknown", parent_permission:true, specialization:)
+        super(age, name: name, parent_permission:parent_permission)
+        @specialization = specialization
+    end
+    def can_use_services?
+        true
+    end
+end
