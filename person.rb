d57a792fee17e186c41f305d51db0001f247da80
FrankMugagga <frank.mugagga@gmail.com>
2023-06-07 19:06:46 +0300

    Remove unnecessary variables

--- person.rb
+++ person.rb
@@ -31,9 +31,3 @@ class Person < Nameable
 
   private :of_age?
 end
-person = Person.new(22, name:'maximilianus')
- puts  person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
- puts capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
- puts capitalizedTrimmedPerson.correct_name
