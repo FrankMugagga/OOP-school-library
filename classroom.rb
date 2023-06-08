efbdf36b49960d4d25e9c139b57ed8d5aefd5ba6
FrankMugagga <frank.mugagga@gmail.com>
2023-06-08 18:28:46 +0300

    Create file for classrom

---
+++ classroom.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+require_relative 'student'
+
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    student.classroom = self
+    @students << student unless @students.include?(student)
+  end
+end
